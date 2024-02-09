248459d4832f1a189a9e26bf7e869ede09c70465
Mahadev Pathak <153206392+mahadevpathak@users.noreply.github.com>
2024-02-09 14:35:39 +0000

    Book Store api package.json file added

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express =require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const bookRoutes =require('./src/book/book.routes')
+
+app.use(express.json())
+app.use('/api/v1/bookRoutes',bookRoutes)
+app.use('/api/v1/userRoutes',userRoutes)
+
+app.get("/",(req,res)=>{
+    res.json("Hello WE ARE HERE !")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(9000,()=>{
+    console.log("Server running on port 9000 !")
 })
